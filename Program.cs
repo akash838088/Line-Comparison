48e1a094d31974d82a1f74a37d188d62831f3be1
Akash Narendra Girase <120698070+akash838088@users.noreply.github.com>
2022-12-17 01:38:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
-﻿namespace LineComparison
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace LineComparision
 {
-    public class Program
+    internal class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WElcome to Line Comparison");
+            int x1;
+            int y1;
+
+            Console.WriteLine("Enter the 1st co-ordinates x1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st co-ordinates y1");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            int x2;
+            int y2;
+
+            Console.WriteLine("Enter the 1st co-ordinates x2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st co-ordinates y2");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length of the line is :- " + length);
+
             Console.ReadLine();
 
         }
